4a1eaed863b1dbc930bf700de6278a430adace2c
gteixe <gteixe@mit.edu>
2018-04-12 15:12:00 -0400

    get parse trees

---
+++ get_parse_tree.py
@@ -0,0 +1,19 @@
+from pycorenlp import StanfordCoreNLP
+nlp = StanfordCoreNLP('http://localhost:9000')
+
+text = []
+
+with open("sentences.txt") as f:
+    for line in f:
+        text.append(line)
+
+print text
+
+
+for s in text:
+    output = nlp.annotate(s, properties={
+        'annotators': 'parse',
+        'outputFormat': 'json'
+    })
+    
+    print(output['sentences'][0]['parse'])
